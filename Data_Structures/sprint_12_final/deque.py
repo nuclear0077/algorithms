dc257a02c2eaf070426eb6e522269925911da44a
Александр Майстришин <nuclear0077@gmail.com>
2023-02-15 11:24:17 +0300

    рефакторинг задачи deque

--- Data_Structures/sprint_12_final/deque.py
+++ Data_Structures/sprint_12_final/deque.py
@@ -1,78 +1,91 @@
-# 82244590
+# 82340964
 # https://contest.yandex.ru/contest/23759/problems/A/
+from typing import List
 
 
-class Decue:
-    def __init__(self, n):
-        self.deque = [None] * n
-        self.max_n = n
-        self.head = 0
-        self.tail = 0
-        self.size = 0
+class EmptyStackError(Exception):
+    pass
 
-    def is_empty(self):
-        return self.size == 0
 
-    def push_back(self, x):
-        if self.size != self.max_n:
-            self.deque[self.tail] = x
-            self.tail = (self.tail + 1) % self.max_n
-            self.size += 1
-            return True
-        return 'error'
+class FullStackError(Exception):
+    pass
 
-    def push_front(self, x):
-        if self.size != self.max_n:
-            self.deque[self.head - 1] = x
-            self.head -= 1
-            self.size += 1
-            return True
-        return 'error'
+
+class Deque:
+    def __init__(self, max_size: int) -> None:
+        self.__deque: List = [None] * max_size
+        self.__max_size: int = max_size
+        self.__head: int = 0
+        self.__tail: int = 0
+        self.__size: int = 0
+
+    def is_empty(self) -> bool:
+        return self.__size == 0
+
+    def is_full(self) -> bool:
+        return self.__size == self.__max_size
+
+    def push_back(self, item) -> None:
+        if self.is_full():
+            raise EmptyStackError('Стек переполнен')
+        self.__deque[self.__tail] = item
+        self.__tail = (self.__tail + 1) % self.__max_size
+        self.__size += 1
+
+    def push_front(self, item) -> None:
+        if self.is_full():
+            raise EmptyStackError('Стек переполнен')
+        self.__deque[self.__head - 1] = item
+        self.__head -= 1
+        self.__size += 1
 
     def pop_front(self):
         if self.is_empty():
-            return 'error'
-        x = self.deque[self.head]
-        self.deque[self.head] = None
-        self.head = (self.head + 1) % self.max_n
-        self.size -= 1
-        return x
+            raise EmptyStackError('Стек пустой')
+        item = self.__deque[self.__head]
+        self.__deque[self.__head] = None
+        self.__head = (self.__head + 1) % self.__max_size
+        self.__size -= 1
+        return item
 
     def pop_back(self):
         if self.is_empty():
-            return 'error'
-        x = self.deque[self.tail - 1]
-        self.deque[self.tail - 1] = None
-        self.size -= 1
-        self.tail -= 1
-        return x
+            raise EmptyStackError('Стек пустой')
+        item = self.__deque[self.__tail - 1]
+        self.__deque[self.__tail - 1] = None
+        self.__size -= 1
+        self.__tail -= 1
+        return item
 
 
-def input_data():
+def get_command() -> List:
     return input().split()
 
 
+def run_deque_method(deque: Deque, command: List):
+    run_command = getattr(deque, command[0])
+    if 'push' not in command[0]:
+        print(run_command())
+    else:
+        run_command(command[1])
+
+
+def run_command(command_amount: int, max_size_deque: int):
+    deque = Deque(max_size_deque)
+    for _ in range(command_amount):
+        try:
+            run_deque_method(deque, get_command())
+        except (EmptyStackError, FullStackError):
+            print('error')
+
+
 def main():
-    n = int(input())
-    m = int(input())
-    deque = Decue(m)
-    answers = []
-    for _ in range(0, n):
-        data = input_data()
-        if data[0] == 'push_front':
-            answer = deque.push_front(int(data[1]))
-            if answer == 'error':
-                answers.append(answer)
-        if data[0] == 'push_back':
-            answer = deque.push_back(int(data[1]))
-            if answer == 'error':
-                answers.append(answer)
-        if data[0] == 'pop_front':
-            answers.append(deque.pop_front())
-        if data[0] == 'pop_back':
-            answers.append(deque.pop_back())
-    for answer in answers:
-        print(answer)
+    command_amount: int = int(input())
+    max_size_deque: int = int(input())
+    try:
+        run_command(command_amount, max_size_deque)
+    except (EmptyStackError, FullStackError):
+        print('error')
 
 
 if __name__ == '__main__':
