d756cbad84f517f7857ef035cfdc708a7be1b14a
Александр Майстришин <nuclear0077@gmail.com>
2023-03-05 10:46:22 +0300

    рефакторинг задачи быстрой сортировки

--- recursion_sorting/sprint_13_final/quick_sort.py
+++ recursion_sorting/sprint_13_final/quick_sort.py
@@ -1,9 +1,25 @@
-# 83354470
+# 83408385
 
-from typing import List, Tuple
+from typing import List, Tuple, Any
 
 
-def partition(data: List[Tuple], left: int, right: int) -> (List[Tuple], int):
+class Participant:
+    def __init__(self, name: str, solved: str, errors: str) -> None:
+        self.name: str = name
+        self.solved: int = -int(solved)
+        self.errors: int = int(errors)
+
+    def __lt__(self, obj):
+        return(
+            (self.solved, self.errors, self.name) <
+            (obj.solved, obj.errors, obj.name)
+        )
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+
+def partition(data: List[Participant], left: int, right: int) -> Tuple[List[Participant], int]:
     pivot = data[(left + right) // 2]
 
     while left <= right:
@@ -24,11 +40,7 @@ def partition(data: List[Tuple], left: int, right: int) -> (List[Tuple], int):
     return data, right
 
 
-def values_conversion(name: str, solved: str, errors: str) -> Tuple:
-    return -int(solved), int(errors), name
-
-
-def quick_sort(data: List[Tuple], left: int, right: int):
+def quick_sort(data: List[Participant], left: int, right: int):
     if left < right:
         data, pivot = partition(data, left, right)
         data = quick_sort(data, left, pivot)
@@ -38,9 +50,9 @@ def quick_sort(data: List[Tuple], left: int, right: int):
 
 def main() -> None:
     amount: int = int(input())
-    data: List[Tuple] = [values_conversion(*input().split()) for _ in range(amount)]
+    data: List[Participant] = [Participant(*input().split()) for _ in range(amount)]
     quick_sort(data, 0, amount - 1)
-    print(*[user[2] for user in data], sep="\n")
+    print(*data, sep="\n")
 
 
 if __name__ == '__main__':
