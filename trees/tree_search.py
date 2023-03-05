c823e4140b1ff31afac5dae0cbc8e6d2b4bd75bc
Александр Майстришин <nuclear0077@gmail.com>
2023-03-05 12:28:45 +0300

    решил задачу с диапазоном

--- trees/tree_search.py
+++ trees/tree_search.py
@@ -30,3 +30,14 @@ def test():
 
 if __name__ == '__main__':
     test()
+
+
+
+
+# функция find_node(root, value):
+#   # Если мы пришли в поддерево, а его не существует, значит нужного элемента в дереве поиска нет
+#   если root == None, то верни "Значение {value} не найдено"
+#   если value < root.value, то верни find_node(root.left, value)
+#   если value = root.value, то верни root 
+#   если value > root.value, то верни find_node(root.right, value)
+#   верни результат 
